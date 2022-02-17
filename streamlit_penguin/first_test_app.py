136906f886c6e31c8b30a9a31a1dd4e19b2e3b5d
ranga4all1 <ranga4all1@gmail.com>
2022-02-17 08:48:09 -0800

    streamlit app for penguins_size.csv dataset

---
+++ streamlit_penguin/first_test_app.py
@@ -0,0 +1,10 @@
+# Create basic streamlit app
+# importing streamlit
+import streamlit as st
+
+# add a title to the page and write in
+st.title("This is my first app with streamlit")
+st.write("This is a simple app to show how streamlit works. Streamlit is an awesome new app that helps to solve the problems quickly.")
+
+# Run this application by using the following command in the terminal.
+# streamlit run first_test_app.py
